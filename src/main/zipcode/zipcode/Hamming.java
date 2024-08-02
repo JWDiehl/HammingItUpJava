379697b4b40d1f8ca70fba2b6493f91b4d5dd527
Jonathan <jondiehl22@gmail.com>
2024-08-02 11:15:18 -0400

    13 out of 15 test pass

---
+++ src/main/zipcode/zipcode/Hamming.java
@@ -0,0 +1,32 @@
+package zipcode;
+
+public class Hamming {
+
+    private String s;
+    private String s1;
+
+
+    public Hamming(String s, String s1) {
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("s must equal s1");
+        }
+        this.s = s;
+        this.s1 = s1;
+    }
+
+    public int getHammingDistance() {
+
+        int distance = 0;
+
+        //Iterate over length of both strands --> only if they are equal lenghths
+
+        for (int i = 0; i < s.length(); i++) {
+            //compare characters at each index
+            if (s.charAt(i) != s1.charAt(i)) {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+}
